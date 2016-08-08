c18437eeaac222ad44e0a3728db22217c2c4888c
VanDeniz <zhibin@cs.uoregon.edu>
2016-08-08 14:15:25 -0700

    testing

--- BirdSimPackage/Main.java
+++ BirdSimPackage/Main.java
@@ -21,7 +21,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -147,7 +147,7 @@ public class Main extends Applet implements Runnable, KeyListener {
     	
     	enemies = new ArrayList<Enemy>();
     	
-    	BufferedReader reader = new BufferedReader(new FileReader(filename));
+    	BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(filename)));
     	while(true){
     		String line = reader.readLine();
     		
